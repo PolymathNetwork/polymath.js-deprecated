603807828520d4b8fe6f70c948d1954e96e4b796
Greg Markou <aidan@chainsafe.io>
2018-01-24 16:44:29 -0500

    Add echo to inform current action

--- scripts/updateContracts.sh
+++ scripts/updateContracts.sh
@@ -1,3 +1,13 @@
-bash ../polymath-core/scripts/autoPull.sh &&
+cd ../polymath-core/scripts &&
+echo "Now in" $PWD
+
+bash autoPull.sh &&
+
+cd ../../polymath.js &&
+echo "Now in" $PWD
+
 cp -r ../polymath-core/contracts . &&
+echo "Copied polymath-core contracts into polymath.js"
+
 rm -rf build
+echo "Removed build directory"
