a4f9ba1ea9cbf46a0370e859bf8542c127d9c677
satyam <satyam@secureblocks.io>
2018-02-14 20:01:06 +0530

    fix migration issues

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,8 @@ const Compliance = artifacts.require('./Compliance.sol');
 const Customers = artifacts.require('./Customers.sol');
 const SecurityTokenRegistrar = artifacts.require('./SecurityTokenRegistrar.sol');
 
-const PolyToken = artifacts.require('./PolyTokenMock.sol');;
+const PolyToken = '0x96a62428509002a7ae5f6ad29e4750d852a3f3d7';
+//artifacts.require('./PolyTokenMock.sol');
 
 module.exports = (deployer, network) => {
   console.log(`Deploying Polymath Network Smart contracts to ${network}...`);
@@ -10,14 +11,13 @@ module.exports = (deployer, network) => {
   // async/await syntax does NOT work here because truffle-deployer returns some
   // weird thing that looks like a promise but has a `then` method with side
   // effects.
-  return deployer.deploy(PolyToken).then(() => {
-    return deployer.deploy(Customers, PolyToken.address);
-  }).then(() => {
+  //return deployer.deploy(PolyToken).then(() => {
+    return deployer.deploy(Customers, PolyToken).then(() => {
     return deployer.deploy(Compliance, Customers.address);
   }).then(() => {
     return deployer.deploy(
       SecurityTokenRegistrar,
-      PolyToken.address,
+      PolyToken,
       Customers.address,
       Compliance.address
     );
